0903b7c4c6f5822c8dfd0802fcf18feca88408e0
jsanabria202 <jsanabria73901@gmail.com>
2024-11-20 21:31:54 -0500

    javascript

---
+++ javascript/01-fundamentos.js
@@ -0,0 +1,12 @@
+//comentario de unica linea
+/*
+esto e sun comentario 
+de java 
+en varias lineas
+*/
+console.log("oi todo mundo!");
+
+/* VARIABLES */
+//declaracion
+var nombre; 
+var nombre2 = "PEPITA"
