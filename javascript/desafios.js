225f8f3023ba981b507b8a9eb537b77d6a490ece
jsanabria202 <jsanabria73901@gmail.com>
2024-11-25 21:44:53 -0500

    desafios 9 y 10

--- javascript/desafios.js
+++ javascript/desafios.js
@@ -21,6 +21,7 @@ presentacion("javier sanabria", 22);
 la función debe imprimir en consola: Su nombre es ____ y usted tiene ____ años,
 si no se recibe la edad imprimir: Su nombre es ____ y usted tiene 100 años.
 */
+/*
 function presentar(nombre, apellido, años = 100) {
   console.log(
     "su nombre es " + nombre + apellido + " y usted tiene " + años + " años"
@@ -28,3 +29,51 @@ function presentar(nombre, apellido, años = 100) {
 }
 presentar("Javier ", "Sanabria", 22);
 presentar("Javier ", "Sanabria");
+*/
+/*8.
+ */
+function suma(a, b) {
+  return a + b;
+}
+function resta(a, b) {
+  return a - b;
+}
+function multiplicacion(a, b) {
+  return a * b;
+}
+function division(a, b) {
+  return a / b;
+}
+function modulo(a, b) {
+  return a % b;
+}
+function potencia(a, b) {
+  return a ** b;
+}
+function ope(operacion, num1, num2) {
+  if (operacion === "suma") {
+    const res = suma(num1, num2);
+    console.log(suma(num1 + " + " + num2 + " = " + res));
+  }
+  if (operacion === "resta") {
+    console.log(resta(num1, num2));
+  }
+  if (operacion === "multiplicacion") {
+    console.log(multiplicacion(num1, num2));
+  }
+  if (operacion === "division") {
+    console.log(division(num1, num2));
+  }
+  if (operacion === "modulo") {
+    console.log(modulo(num1, num2));
+  }
+  if (operacion === "potencia") {
+    console.log(potencia(num1, num2));
+  }
+}
+ope("suma", 5, 5);
+ope("resta", 5, 5);
+ope("multiplicacion", 5, 5);
+ope("division", 5, 5);
+ope("modulo", 5, 5);
+ope("potencia", 5, 5);
